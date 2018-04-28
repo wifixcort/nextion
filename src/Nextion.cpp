0b3cf316c8b5dd1471da05a310f4d366a64adc11
Ricardo Mena C <rgacort12@gmail.com>
2018-04-28 13:38:52 -0600

    Changed from Serial to Stream
    Signed-off-by: Ricardo Mena C <rgacort12@gmail.com>

--- src/Nextion.cpp
+++ src/Nextion.cpp
@@ -11,23 +11,23 @@ http://crcibernetica.com
 
  License
  **********************************************************************************
- This program is free software; you can redistribute it 
- and/or modify it under the terms of the GNU General    
- Public License as published by the Free Software       
- Foundation; either version 3 of the License, or        
- (at your option) any later version.                    
-                                                        
- This program is distributed in the hope that it will   
- be useful, but WITHOUT ANY WARRANTY; without even the  
- implied warranty of MERCHANTABILITY or FITNESS FOR A   
- PARTICULAR PURPOSE. See the GNU General Public        
- License for more details.                              
-                                                        
- You should have received a copy of the GNU General    
+ This program is free software; you can redistribute it
+ and/or modify it under the terms of the GNU General
+ Public License as published by the Free Software
+ Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will
+ be useful, but WITHOUT ANY WARRANTY; without even the
+ implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE. See the GNU General Public
+ License for more details.
+
+ You should have received a copy of the GNU General
  Public License along with this program.
  If not, see <http://www.gnu.org/licenses/>.
-                                                        
- Licence can be viewed at                               
+
+ Licence can be viewed at
  http://www.gnu.org/licenses/gpl-3.0.txt
 
  Please maintain this license information along with authorship
@@ -37,17 +37,15 @@ http://crcibernetica.com
 
 #include "Nextion.h"
 
-#if defined(USE_SOFTWARE_SERIAL)
-Nextion::Nextion(SoftwareSerial &next, uint32_t baud): nextion(&next){
-  nextion->begin(baud);
-  flushSerial();
-}
-#else
-Nextion::Nextion(HardwareSerial &next, uint32_t baud): nextion(&next){
-  nextion->begin(baud);
-  flushSerial();
-}
-#endif
+// #if defined(USE_SOFTWARE_SERIAL)
+// Nextion::Nextion(SoftwareSerial &next, uint32_t baud): nextion(&next){
+//   nextion.begin(baud);
+//   flushSerial();
+// }
+// #else
+// Nextion::Nextion(HardwareSerial &next, uint32_t baud): nextion(&next){
+Nextion::Nextion(Stream &sPort):nextion(sPort){}
+// #endif
 
 void Nextion::buttonToggle(boolean &buttonState, String objName, uint8_t picDefualtId, uint8_t picSelected){
   String tempStr = "";
@@ -66,7 +64,7 @@ void Nextion::buttonToggle(boolean &buttonState, String objName, uint8_t picDefu
   }
 }//end buttonPressed
 
-uint8_t Nextion::buttonOnOff(String find_component, String unknown_component, uint8_t pin, int btn_prev_state){  
+uint8_t Nextion::buttonOnOff(String find_component, String unknown_component, uint8_t pin, int btn_prev_state){
   uint8_t btn_state = btn_prev_state;
   if((unknown_component == find_component) && (!btn_state)){
     btn_state = 1;//Led is ON
@@ -90,15 +88,15 @@ boolean Nextion::setComponentValue(String component, int value){
 boolean Nextion::ack(void){
   /* CODE+END*/
   uint8_t bytes[4] = {0};
-  nextion->setTimeout(20);
-  if (sizeof(bytes) != nextion->readBytes((char *)bytes, sizeof(bytes))){
+  nextion.setTimeout(20);
+  if (sizeof(bytes) != nextion.readBytes((char *)bytes, sizeof(bytes))){
     return 0;
   }//end if
   if((bytes[1]==0xFF)&&(bytes[2]==0xFF)&&(bytes[3]==0xFF)){
     switch (bytes[0]) {
 	case 0x00:
 	  return false; break;
-	  //return "0"; break;      
+	  //return "0"; break;
 	case 0x01:
 	  return true; break;
 	  //return "1"; break;
@@ -112,7 +110,7 @@ boolean Nextion::ack(void){
 	  return "1A"; break;
 	case 0x1B:
 	  return "1B"; break;//*/
-	default: 
+	default:
 	  return false;
     }//end switch
   }//end if
@@ -123,8 +121,8 @@ unsigned int Nextion::getComponentValue(String component){
     uint32_t value = 0;
   sendCommand(getValue.c_str());
   uint8_t temp[8] = {0};
-  nextion->setTimeout(20);
-  if (sizeof(temp) != nextion->readBytes((char *)temp, sizeof(temp))){
+  nextion.setTimeout(20);
+  if (sizeof(temp) != nextion.readBytes((char *)temp, sizeof(temp))){
     return -1;
   }//end if
   if((temp[0]==(0x71))&&(temp[5]==0xFF)&&(temp[6]==0xFF)&&(temp[7]==0xFF)){
@@ -155,10 +153,10 @@ boolean Nextion::updateProgressBar(int x, int y, int maxWidth, int maxHeight, in
 	h2 = maxHeight;
 	offset1 = x + value;
 	offset2 = y;
-	
+
 	}else{ // vertical
 	value = map(value, 0, 100, 0, maxHeight);
-	offset2 = y;	
+	offset2 = y;
 	y = y + maxHeight - value;
 	w1 = maxWidth;
 	h1 = value;
@@ -166,7 +164,7 @@ boolean Nextion::updateProgressBar(int x, int y, int maxWidth, int maxHeight, in
 	h2 = maxHeight - value;
 	offset1 = x;
 	}//end if
-	
+
 	String wipe = "picq " + String(x) + "," + String(y) + "," + String(w1) + "," + String(h1) + "," + String(fullPictureID);
 	sendCommand(wipe.c_str());
 	wipe = "picq " + String(offset1) + "," + String(offset2) + "," + String(w2) + "," + String(h2) + "," + String(emptyPictureID);
@@ -186,8 +184,8 @@ String Nextion::getComponentText(String component, uint32_t timeout){
   /*unsigned long start = millis();
   uint8_t ff = 0;//end message
   while((millis()-start < timeout)){
-    if(nextion->available()){
-      char b = nextion->read();
+    if(nextion.available()){
+      char b = nextion.read();
       if(String(b, HEX) == "ffff"){ff++;}
        tempStr += String(b);
 	   if(ff == 3){//End line
@@ -211,10 +209,10 @@ String Nextion::listen(unsigned long timeout){//returns generic
   String cmd;
   int countEnd = 0;
 
-  while(nextion->available()>0){
+  while(nextion.available()>0){
 	delay(10);
-	if(nextion->available()>0){
-	  _bite = nextion->read();
+	if(nextion.available()>0){
+	  _bite = nextion.read();
 	  cmd += _bite;
 	  if(_bite == _end){
 		countEnd++;
@@ -255,7 +253,7 @@ String Nextion::listen(unsigned long timeout){//returns generic
 	break;
   case 'h'://0x68
 	cmd = String(cmd[2], DEC) + "," + String(cmd[4], DEC) +","+ String(cmd[5], DEC);
-	cmd = "68 " + cmd;	
+	cmd = "68 " + cmd;
 	return cmd;
 	break;
   case 'p'://0x70
@@ -263,13 +261,13 @@ String Nextion::listen(unsigned long timeout){//returns generic
 	cmd = "70 " + cmd;
 	return cmd;
 	break;
-  default: 
+  default:
 	//	cmd += String(b, HEX);
 	//if(ff == 3){break;}//end if
 	//cmd += " ";//
 	return cmd;//
 	break;
-  }//end switch	
+  }//end switch
   return "";
 }//end listen
 
@@ -282,10 +280,10 @@ String Nextion::listen(unsigned long timeout){//returns generic
   int countEnd = 0;
   unsigned long start = millis();
 
-  while(nextion->available()>0){
+  while(nextion.available()>0){
 	delay(10);
-	if(nextion->available()>0){
-	  _bite = nextion->read();
+	if(nextion.available()>0){
+	  _bite = nextion.read();
 	  cmd += String(_bite, HEX);
 	  if(_bite == _end){
 		countEnd++;
@@ -313,17 +311,17 @@ uint8_t Nextion::pageId(void){
 	return _pageId.toInt();
   }
   return -1;
-  
+
 }//pageId
 
 void Nextion::sendCommand(const char* cmd){
-  while (nextion->available()){
-	nextion->read();
+  while (nextion.available()){
+	nextion.read();
   }//end while
-  nextion->print(cmd);
-  nextion->write(0xFF);
-  nextion->write(0xFF);
-  nextion->write(0xFF);
+  nextion.print(cmd);
+  nextion.write(0xFF);
+  nextion.write(0xFF);
+  nextion.write(0xFF);
 }//end sendCommand
 
 boolean Nextion::init(const char* pageId){
@@ -337,7 +335,5 @@ boolean Nextion::init(const char* pageId){
 
 void Nextion::flushSerial(){
   Serial.flush();
-  nextion->flush();
+  nextion.flush();
 }//end flush
-
- 
--- src/Nextion.h
+++ src/Nextion.h
@@ -14,23 +14,23 @@ http://crcibernetica.com
 
  License
  **********************************************************************************
- This program is free software; you can redistribute it 
- and/or modify it under the terms of the GNU General    
- Public License as published by the Free Software       
- Foundation; either version 3 of the License, or        
- (at your option) any later version.                    
-                                                        
- This program is distributed in the hope that it will   
- be useful, but WITHOUT ANY WARRANTY; without even the  
- implied warranty of MERCHANTABILITY or FITNESS FOR A   
- PARTICULAR PURPOSE. See the GNU General Public        
- License for more details.                              
-                                                        
- You should have received a copy of the GNU General    
+ This program is free software; you can redistribute it
+ and/or modify it under the terms of the GNU General
+ Public License as published by the Free Software
+ Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will
+ be useful, but WITHOUT ANY WARRANTY; without even the
+ implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE. See the GNU General Public
+ License for more details.
+
+ You should have received a copy of the GNU General
  Public License along with this program.
  If not, see <http://www.gnu.org/licenses/>.
-                                                        
- Licence can be viewed at                               
+
+ Licence can be viewed at
  http://www.gnu.org/licenses/gpl-3.0.txt
 
  Please maintain this license information along with authorship
@@ -42,30 +42,34 @@ http://crcibernetica.com
 #define __NEXTION_H__
 
 #include <Arduino.h>
+// #include <Stream.h>
 
 
-#define USE_SOFTWARE_SERIAL //Comment this line for use  HardwareSerial
+// #define USE_SOFTWARE_SERIAL //Comment this line for use  HardwareSerial
 
 
-#if defined(USE_SOFTWARE_SERIAL)
-#include <SoftwareSerial.h>
-#endif
+// #if defined(USE_SOFTWARE_SERIAL)
+// #include <SoftwareSerial.h>
+// #endif
 
 class Nextion{
  private:
   void flushSerial();
-#if defined(USE_SOFTWARE_SERIAL)
-  SoftwareSerial *nextion;
-#else
-   HardwareSerial *nextion;
-#endif
+  Stream &nextion;
+// #if defined(USE_SOFTWARE_SERIAL)
+//   SoftwareSerial *nextion;
+// #else
+//    HardwareSerial *nextion;
+// #endif
  public:
-    Nextion(){};//Empty contructor
-#if defined(USE_SOFTWARE_SERIAL)
-  Nextion(SoftwareSerial &next, uint32_t baud);//Constructor
-#else
-  Nextion(HardwareSerial &next, uint32_t baud);//Constructor
-#endif
+    // Nextion(){};//Empty contructor
+    Nextion(Stream &sPort);
+    // Nextion(Stream &sPort):nextion(sPort){}
+// #if defined(USE_SOFTWARE_SERIAL)
+//   Nextion(SoftwareSerial &next, uint32_t baud);//Constructor
+// #else
+//   Nextion(HardwareSerial &next, uint32_t baud);//Constructor
+// #endif
   void buttonToggle(boolean &buttonState, String objName, uint8_t picDefualtId, uint8_t picPressedId);
 
   uint8_t buttonOnOff(String find_component, String unknown_component, uint8_t pin, int btn_prev_state);
@@ -79,7 +83,7 @@ class Nextion{
   unsigned int getComponentValue(String component);
 
   boolean setComponentText(String component, String txt);
-  
+
   boolean updateProgressBar(int x, int y, int maxWidth, int maxHeight, int value, int emptyPictureID, int fullPictureID, int orientation=0);
 
   String getComponentText(String component, uint32_t timeout = 100);
@@ -95,9 +99,3 @@ class Nextion{
 
 };
 #endif
-
-
-
-
-
-
